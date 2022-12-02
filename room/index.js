8f22ae293556ba9db290c106ab966146ba75184c
Prashant Kumar <pk2psp@gmail.com>
2022-12-02 23:49:29 +0530

    Rooms added in Socket

---
+++ room/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+    3034: {
+        name: 'lounge',
+        members: []
+    }, 3035: {
+        name: 'crasher',
+        members: []
+    }, 3036: {
+        name: 'huka baar',
+        members: []
+    }, 3037: {
+        name: 'warfront',
+        members: []
+    }, 3038: {
+        name: 'decker',
+        members: []
+    },
+}
